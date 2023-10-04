299a7d72d0aa9734ace9bcfc46fb89a727490a38
mohamed501234 <hama.bennacef50@gmail.com>
2023-10-04 18:10:05 +0100

    done

---
+++ Firent/Firent/API.js
@@ -0,0 +1,3 @@
+const ADRESS_API="192.168.11.233"
+
+export default ADRESS_API
--- Firent/Firent/App.js
+++ Firent/Firent/App.js
@@ -3,6 +3,7 @@ import { StatusBar, Image, View, StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Signin from "./Screens/Signin";
+import Signup from "./Screens/Signup";
 import Splash from "./Screens/Splash";
 import Login from "./Screens/login";
 import checkEmail from "./Screens/checkEmail";
@@ -13,13 +14,11 @@ import { User, onAuthStateChanged } from "firebase/auth";
 import { FIREBASE_AUTH } from "./FireBase";
 const Stack = createStackNavigator();
 const InsideStack = createStackNavigator();
-// import Signin from "./Screens/Signin";
-import Signup from "./Screens/Signup"
 
 function InsideLayout() {
   return (
     <InsideStack.Navigator>
-      <InsideStack.Screen name="todos" component={list} />
+      <InsideStack.Screen name="todos" component={Splash} />
       <InsideStack.Screen name="details" component={details} />
     </InsideStack.Navigator>
   );
@@ -37,19 +36,24 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LocationDetails">
         {user ? (
-          <Stack.Screen name="Inside" component={insideLayout} options={{headerShown: false}} />
+          <Stack.Screen
+            name="Inside"
+            component={insideLayout}
+            options={{ headerShown: false }}
+          />
         ) : (
           <>
-                      <Stack.Screen name="checkEmail" component={checkEmail}/> 
+            <Stack.Screen name="checkEmail" component={checkEmail} />
 
-            {/* <Stack.Screen name="Splash" component={Splash} /> */}
+            <Stack.Screen name="Splash" component={Splash} />
             <Stack.Screen name="Signin" component={Signin} />
+            <Stack.Screen name="Signup" component={Signup} />
 
-
-
-            <Stack.Screen name="login" component={Login} options={{headerShown:false}} /> 
-
-
+            <Stack.Screen
+              name="login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
           </>
         )}
       </Stack.Navigator>
