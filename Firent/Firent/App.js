5972fbf32b337295d153912ca05909514f45208f
mohamed501234 <hama.bennacef50@gmail.com>
2023-10-19 15:43:19 +0100

    edit profile / chat

--- Firent/Firent/App.js
+++ Firent/Firent/App.js
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { StatusBar, Image, View, StyleSheet } from "react-native";
+// import { StatusBar, Image, View, StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
-
+import ADRESS_API from "./API";
 // Screens
 import Signin from "./Screens/Signin";
 import Signup from "./Screens/Signup";
 import Chat from "./Screens/Chat";
-import Login from "./Screens/login";
+// import Login from "./Screens/login";
 import homePage from "./Screens/homePage";
 import checkEmail from "./Screens/checkEmail";
 import LocationDetails from "./Screens/locationDetails";
-import list from "./Screens/list";
-import { FIREBASE_AUTH } from "./FireBase";
+// import list from "./Screens/list";
+// import { FIREBASE_AUTH } from "./FireBase";
 import Conversation1 from "./Screens/Conversation1";
 import EditProfile from "./Screens/EditProfile";
 import ThreadList from "../Firent/components/forum/ThreadListItem";
 import Cart from "./Screens/Cart";
 import CommentListItem from "../Firent/components/forum/CommonListItem";
 import HpUserNotConnected from "./Screens/HpUserNotConnected";
-import Conversation from "./Screens/Conversation";
+// import Conversation from "./Screens/Conversation";
 import Payment1 from "./Screens/Payment1";
 import Payment2 from "./Screens/Payment2";
 import Payment3 from "./Screens/payment3";
@@ -41,51 +41,16 @@ export default function App() {
         {user ? (
           <>
             <Stack.Screen
-                  name="EditProfile"
-                  component={EditProfile}
-                  initialParams={{ userGetter, firebaseId: user.id }}
-                  options={{ headerShown: false }}
-           />
-            <Stack.Screen
-                  name="homePage"
-                  component={homePage}
-                  options={{ headerShown: false }}
-            />
-            <Stack.Screen
               name="Conversation1"
               component={Conversation1}
               initialParams={{ user }}
               options={{ headerShown: false }}
             />
-            <Stack.Screen
-              name="ImageGrid"
-              component={ImageGrid}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Userprofilimages"
-              component={Userprofilimages}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="HpUserNotConnected"
-              component={HpUserNotConnected}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="LocationDetails"
-              component={LocationDetails}
-              options={{ headerShown: false }}
-            />
             <Stack.Screen name="Chat" component={Chat} />
             <Stack.Screen
-              name="Payment1"
-              component={Payment1}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="Payment2"
-              component={Payment2}
+              name="EditProfile"
+              component={EditProfile}
+              initialParams={{ user }}
               options={{ headerShown: false }}
             />
             <Stack.Screen
@@ -99,10 +64,50 @@ export default function App() {
               options={{ headerShown: false }}
             />
             <Stack.Screen
+              name="LocationDetails"
+              component={LocationDetails}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Userprofilimages"
+              component={Userprofilimages}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+              name="Payment2"
+              component={Payment2}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Payment1"
+              component={Payment1}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+              name="homePage"
+              component={homePage}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="ImageGrid"
+              component={ImageGrid}
+              options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+              name="HpUserNotConnected"
+              component={HpUserNotConnected}
+              options={{ headerShown: false }}
+            />
+
+
+            <Stack.Screen
               name="Threads"
               component={ThreadList}
               options={{ title: "Forum Threads" }}
-            /> 
+            />
             <Stack.Screen
               name="Comments"
               component={CommentListItem}
@@ -111,7 +116,11 @@ export default function App() {
           </>
         ) : (
           <>
-          <Stack.Screen name="Signup" component={Signup} />
+            <Stack.Screen
+              name="Signup"
+              component={Signup}
+              options={{ headerShown: false }}
+            />
             <Stack.Screen
               name="Signin"
               component={Signin}
@@ -122,7 +131,7 @@ export default function App() {
               name="checkEmail"
               component={checkEmail}
               options={{ headerShown: false }}
-          />
+            />
           </>
         )}
       </Stack.Navigator>
